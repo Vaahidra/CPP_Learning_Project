0cd66b41b9545d4cb0625c02ef75b6aab2b2a223
REDJRADJ <redjradj.wahid99@gmail.com>
2022-03-21 11:40:58 +0100

    TASK_1 clean

---
+++ src/AircraftManager.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <ostream>
+#include <vector>
+#include <memory>
+#include "aircraft.hpp"
+#include "GL/dynamic_object.hpp"
+
+class Aircraft;
+
+class AircraftManager : public GL::DynamicObject
+{
+public:
+    AircraftManager();                       // Base Constructor
+    ~AircraftManager() override = default;   // Destructor
+    AircraftManager(const AircraftManager&) = delete;
+    AircraftManager& operator=(const AircraftManager&) = delete;
+    
+    void add_aircraft(std::unique_ptr<Aircraft>);
+    void move(double) override;
+
+private:
+    std::vector<std::unique_ptr<Aircraft>> aircrafts;
+    bool move_aircraft(double dt, Aircraft &craft);
+};
